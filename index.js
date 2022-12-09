2fc12b6850802df761301d5b837600fb21477cf9
imtiazho <axubair9@gmail.com>
2022-12-09 21:12:32 +0600

    Complete with dashboard and save register and jwt token

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
+const jwt = require("jsonwebtoken");
 
 // Middleware
 app.use(cors());
@@ -16,6 +17,21 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+function verifyJWT(req, res, next) {
+  const authHeaders = req.headers.authorization;
+  if (!authHeaders) {
+    return res.status(401).send({ message: "Anauthorize Access" });
+  }
+  const token = authHeaders.split(" ")[1];
+  jwt.verify(token, process.env.SECRET_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ Messgae: "Access forbidden" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 async function run() {
   try {
     await client.connect();
@@ -25,6 +41,57 @@ async function run() {
     const bookingCollection = client
       .db("doctors_portal")
       .collection("bookings");
+    const usersCollection = client.db("doctors_portal").collection("users");
+
+    // get all users
+    app.get("/allUsers", verifyJWT, async (req, res) => {
+      const query = {};
+      const users = usersCollection.find(query);
+      const cursor = await users.toArray();
+      res.send(cursor);
+    });
+
+    // Handle user
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      const token = jwt.sign({ email: email }, process.env.SECRET_TOKEN, {
+        expiresIn: "1h",
+      });
+      res.send({ result, token });
+    });
+
+    // Handle Admin
+    app.put("/user/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const requester = req.decoded.email;
+      const requesterAccount = await usersCollection.findOne({
+        email: requester,
+      });
+      if (requesterAccount?.role === "admin") {
+        const filter = { email: email };
+        const updateDoc = {
+          $set: { role: "admin" },
+        };
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        const token = jwt.sign({ email: email }, process.env.SECRET_TOKEN, {
+          expiresIn: "1h",
+        });
+        res.send({ result, token });
+      } else {
+        return res.status(403).send({ messgae: "Forbidden" });
+      }
+    });
 
     // Get all Service
     app.get("/allServices", async (req, res) => {
@@ -32,6 +99,21 @@ async function run() {
       const allServices = doctorPortalsCollection.find(query);
       const cursor = await allServices.toArray();
       res.send(cursor);
+    });
+
+    // get all specific user booking
+    app.get("/booking", verifyJWT, async (req, res) => {
+      const email = req.query.patientEmail;
+      const decodedEmail = req.decoded.email;
+
+      if (decodedEmail === email) {
+        const query = { patientEmail: email };
+        const result = bookingCollection.find(query);
+        const cursor = await result.toArray();
+        res.send(cursor);
+      } else {
+        return res.status(403).send({ message: "Forbidden Access" });
+      }
     });
 
     // Warning this is not proper way to filtering something. After you learn you use agreegate lookup
@@ -63,6 +145,7 @@ async function run() {
       });
       res.send(avaiableServices);
     });
+
     // Post Booking Data
     app.post("/bookings", async (req, res) => {
       const booking = req.body;
@@ -78,6 +161,14 @@ async function run() {
       }
       const result = await bookingCollection.insertOne(booking);
       return res.send({ success: true, result });
+    });
+
+    // Chekc admin
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await usersCollection.findOne({ email: email });
+      const isAdmin = user?.role === "admin";
+      res.send({ admin: isAdmin });
     });
   } finally {
     // client.close();
